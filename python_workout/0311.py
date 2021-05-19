5679a38a1a33cb48eac21d8d6e8039f98b52e8b4
Jon <jonwhittlestone@users.noreply.github.com>
2021-05-19 06:44:44 +0000

    Chapter 03 - Exercise 11 Sorting names in a list of dicts with `itemgetter`

---
+++ python_workout/0311.py
@@ -0,0 +1,27 @@
+import pprint
+import operator
+print("Chapter 03 - Exercise 11")
+print("")
+
+# Exercise specifies using itemgetter
+#   https://docs.python.org/3/library/operator.html?#operator.itemgetter
+
+PEOPLE = [{'first':'Jon', 'last':'Whittlestone',
+    'email':'jwhittlestone@hotmail.com'},
+ {'first':'Joe', 'last':'Biden',
+    'email':'president@whitehouse.gov'},
+ {'first':'Vladimir', 'last':'Putin',
+    'email':'president@kremvax.ru'}
+ ]
+
+def alphabetize_names(list_of_dicts):    
+    return sorted(list_of_dicts,        
+                key=operator.itemgetter('last', 'first')) 
+
+if __name__ == "__main__":
+    print('Unsorted:')
+    pprint.pprint(PEOPLE)
+
+    print('Sorted:')
+    sorted_result = alphabetize_names(PEOPLE)
+    pprint.pprint(sorted_result)
