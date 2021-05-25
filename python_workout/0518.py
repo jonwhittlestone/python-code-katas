d4cc836fb1eae550a49fb7b82d9161e3495a425d
Jon <jonwhittlestone@users.noreply.github.com>
2021-05-25 14:00:40 +0000

    Chapter 05 - Exercise 18 writing last line of a text file

---
+++ python_workout/0518.py
@@ -0,0 +1,18 @@
+import os
+print('Chapter 05 - Exercise 18')
+print('')
+print('Reading a file. and returning the final line.')
+
+
+def get_final_line(thefile: str) -> str:
+    with open(thefile) as f:
+        contents = f.read()
+        lines = contents.split("\n")
+
+    return lines[-1]
+
+
+if __name__ == '__main__':
+    thefile = '/workspaces/python-workout/python_workout/supporting_files/fun.txt'
+    final_line = get_final_line(thefile)
+    print(final_line)
