48b171c4757bf5b4f73009e64a6bcb28f0aba4f0
Jon <jonwhittlestone@users.noreply.github.com>
2021-05-29 06:59:11 +0000

    Chapter 05 - Exercise 22 - reading with pathlib, writing a csv with csv.writer

---
+++ python_workout/0522.py
@@ -0,0 +1,29 @@
+import csv
+import os
+import pathlib
+print('Chapter 05 - Exercise 22')
+print('')
+print('Reading and writing CSV')
+
+
+def passwd_to_csv(in_file, out_file):
+    p = pathlib.Path(in_file)
+    contents_lines = p.read_text().split('\n')
+    with open(out_file, 'w') as f:
+        o = csv.writer(f, delimiter="\t")
+        for ln in contents_lines:
+            row = ln.split(':')
+            if len(row) > 1:
+                o.writerow([row[0], row[2]])
+            debug = True
+
+    print(f"Finished writing to {out_file}")
+
+
+if __name__ == '__main__':
+    DIR = os.path.join('.', 'python_workout',
+                       'supporting_files')
+    in_file = os.path.join(DIR, 'passwd')
+    out_file = os.path.join(DIR, 'passwd.csv')
+
+    passwd_to_csv(in_file, out_file)
